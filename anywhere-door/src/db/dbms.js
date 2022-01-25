27851ea7498f50f0df13c2371c1e130713bd8e99
Aniket-coder29 <aniket.balodia.ug20@nsut.ac.in>
2022-01-25 21:28:36 +0530

    for Mongodb_cloud

--- anywhere-door/src/db/dbms.js
+++ anywhere-door/src/db/dbms.js
@@ -1,5 +1,6 @@
 const mongoose= require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/RegisteredUsers",{
+const mongoDbUrl= process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/RegisteredUsers";
+mongoose.connect(mongoDbUrl,{
     useNewUrlParser:true,
     useUnifiedTopology:true
     // useCreateIndex:true
